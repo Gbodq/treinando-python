3147ea5f9b499f441a8fd2e6e699752724dffdf0
Gbodq <dylanfranca@outlook.com>
2023-12-11 21:42:41 -0300

    70% do exercicio 1 concluido

---
+++ Capitulo 5/01-testes_condicionais.py
@@ -0,0 +1,35 @@
+car = 'BMW'
+# Onde 5 comparação dão false e 5 dão true
+# for i in range(1, 10):
+#   1. printar a comparação em texto e qual você acha que será o resultado
+#   2. printar o resultado da comparação
+
+print(f"car == BMW is True")
+print(car == "BMW")
+
+cat = "gato"
+print("cat == cachorro é falsa")
+print(cat == "cachorro")
+
+crow = "ave"
+print("crow == ave is true")
+print(crow == "ave")
+
+crown = "coroa"
+print("crown == crow is false")
+print(crown == crow)
+
+verde = "vermelho"
+print(f"verde == vermelho is {verde == 'vermelho'}")
+
+
+luz = "energia"
+print(f"luz == escuridao is {luz == 'escuridao'}")
+
+
+raio = "tempestade"
+print(f"raio == tempestade is {raio == 'tempestade'}")
+
+
+
+
