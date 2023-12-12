7eff902170a2b62d95a8aaaacf4fa10b6a6eb463
Gbodq <dylanfranca@outlook.com>
2023-12-11 21:42:21 -0300

    teste com if

---
+++ Capitulo 5/teste_if.py
@@ -0,0 +1,13 @@
+convidados =   ['luke', 
+                'jen', 
+                'tom',
+                'raquel',
+                'derek', 
+                'paula',
+                'chico']
+
+numeros = [8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+
+number = 10
+if number > 5:
+    print('que número grande!')
