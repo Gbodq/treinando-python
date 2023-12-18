d15f16c0dea1b0e2ec89f180e4299eda3e3d2d48
Gbodq <dylanfranca@outlook.com>
2023-12-18 18:56:21 -0300

    corrigindo testes condicionais

--- Capitulo 5/1_testes condicionais.py
+++ Capitulo 5/1_testes condicionais.py
@@ -6,16 +6,16 @@ print("\n car == 'audi' is false")
 print(car == "audi")
 
 cat = "gato"
-print(f"cat == 'gato' is {cat == "gato"}")
-print(f"cat == 'dog' is {cat == "dog"}\n")
+print(f"cat == 'gato' is {cat == 'gato'}")
+print(f"cat == 'dog' is {cat == 'dog'}\n")
 
 green = "verde"
-print(f"green == 'verde' is {green == "verde"} ")
-print(f"green == 'vermelho' is {green == "vermelho"}\n")
+print(f"green == 'verde' is {green == 'verde'} ")
+print(f"green == 'vermelho' is {green == 'vermelho'}\n")
 
 home = "casa"
-print(f"home == 'casa' is {home == "casa"}")
-print(f"home == 'rua' is {home == "rua"}\n")
+print(f"home == 'casa' is {home == 'casa'}")
+print(f"home == 'rua' is {home == 'rua'}\n")
 
 number = 42
 print(f"number >= 42 is {number >= 42}")
