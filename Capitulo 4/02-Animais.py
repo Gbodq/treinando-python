e1a19717e1855f8c7a9140ee17603c243e9ae457
Dylan Pereira <153128056+Gbodq@users.noreply.github.com>
2023-12-26 16:13:05 -0300

    Delete Capitulo 4 directory

--- Capitulo 4/02-Animais.py
+++
@@ -1,7 +0,0 @@
-pets = ["dog",'cat',"rabbit"]
-
-for pet in pets:
-    print(pet.title())
-    print(f"A {pet.title()} would make a great pet")
-
-print("ANY OF THEM WOULD MAKE A GREAT PET")
--- Capitulo 4/08-cubos.py
+++
@@ -1,4 +0,0 @@
-cubes = []
-for number in range(1,11):
-    cubes.append(number**3)
-    print(cubes)
