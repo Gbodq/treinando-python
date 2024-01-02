a04778725e322286eb8850a0aeb6a033a02b1dbb
Dylan Pereira <dylanfranca@outlook.com>
2024-01-02 15:50:38 -0300

    acessando dicionário

---
+++ capitulo 6/1- Pessoas.py
@@ -0,0 +1,6 @@
+person = {'first_name':'Derick','last_name':'Pereira','age':14,'city':'Rio de Janeiro'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
