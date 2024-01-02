3c3f7cc36a219f043aeeb3e9beb13d2823c2cab2
Dylan Pereira <dylanfranca@outlook.com>
2024-01-02 16:04:47 -0300

    acessando dicionário

---
+++ capitulo 6/2-número_favorito.py
@@ -0,0 +1,7 @@
+números = {'Luke':42,'Dylan':33,'Derick':10,'Ana':15,'Francisco':50}
+
+print(números['Luke'])
+print(números['Dylan'])
+print(números['Derick'])
+print(números['Ana'])
+print(números['Francisco'])
