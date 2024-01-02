8266f6a06cbd231079235a36a9dbfdc026bc746a
Dylan Pereira <dylanfranca@outlook.com>
2024-01-02 17:05:54 -0300

    acessando dicionário

---
+++ capitulo 6/3-glossário.py
@@ -0,0 +1,11 @@
+glossário = {'strings': 'sequencia de caracteres envolta em aspas',
+             'booleano':'utilizado para provar valores em verdadeiro ou falso',
+             'variavel': 'salvar um valor, que sera usado pelo programa',
+             'lista' : 'armazenar um conjunto de valores em uma variável',
+             'dicionário' : 'permitem conectar informações relacionadas'}
+
+print(f"strings: {glossário['strings']} \n")
+print(f"booleano: {glossário['booleano']} \n")
+print(f"variavel:{glossário['variavel']} \n")
+print(f"lista: {glossário['lista']} \n")
+print(f"dicionário: {dicionário['dicionário']}")
