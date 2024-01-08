26a42d84a6303edc3d94dd639e559a1048b1bd43
Dylan Pereira <dylanfranca@outlook.com>
2024-01-08 10:33:28 -0300

    treinando o bubble

--- algoritmo/bubble.py
+++ algoritmo/bubble.py
@@ -1,4 +1,8 @@
 numbers = [2,8,6,10,7,5,9,4,3,1]
+n = len(numbers)
 
-for pos_inicial in range(n):
-        ad
+for pos_inicial in range(n-1):
+    for pos_atual in range(n-1-pos_inicial):
+      if numbers[pos_atual] > numbers[pos_atual+1]:
+        numbers[pos_atual],numbers[pos_atual+1] = numbers[pos_atual+1],numbers[pos_atual]
+print(numbers)
